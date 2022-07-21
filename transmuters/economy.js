8465db3dd90c3270affddb987e4be0e71f5a1c89
Vivek Maurya <vivekcmaurya30@gmail.com>
2022-07-22 00:03:01 +0530

    Added a bunch of economy fields

--- transmuters/economy.js
+++ transmuters/economy.js
@@ -1,5 +1,5 @@
 const { toSnakeCase } = require("../formatter");
-const { findFieldById, transmuteHtmlToPlain, getNoteIfExists, findSubfieldByName } = require("../transmuter");
+const { findFieldById, transmuteHtmlToPlain, getNoteIfExists, findSubfieldByName, transmuteValueUnitDateField } = require("../transmuter");
 
 
 const ID = "economy";
@@ -17,9 +17,26 @@ function economy(category) {
                 'by_sector_of_origin': gdp_composition_by_sector_of_origin(category),
             },
         },
+        'credit_ratings': credit_ratings(category),
         'agricultural_products': agricultural_products(category),
         'industries': industries(category),
         'industrial_production_growth_rate': industrial_production_growth_rate(category),
+        'labor_force': {
+            'total_size': labor_force_total_size(category),
+            'by_occupation': labor_force_by_occupation(category),
+        },
+        'unemployment_rate': {
+            'overall': unemployment_rate_overall(category),
+            'in_youth_ages_15_to_24': unemployment_rate_in_youth_ages_15_to_24(category),
+        },
+        'population_below_poverty_line': population_below_poverty_line(category),
+        'gini_index_coefficient': gini_index_coefficient(category),
+        'household_income_by_percentage_share': household_income_by_percentage_share(category),
+        'budget': budget(category),
+        'taxes_and_other_revenues': taxes_and_other_revenues(category),
+        'budget_surplus_or_deficit': budget_surplus_or_deficit(category),
+        'public_debt': public_debt(category),
+        'fiscal_year': fiscal_year(category),
     };
 }
 
@@ -92,6 +109,10 @@ function gdp_composition_by_end_use(category) {
     if (!field) return null;
     if (!field.subfields) return transmuteHtmlToPlain(field.content);
 
+    // Ian's JSON presents only one date for the entire field
+    // Even though the dates may exist separately and independently
+    const datedSubfield = field.subfields.find(subfield => subfield.info_date);
+
     const endUses = Object.fromEntries(
         field.subfields
              .map(subfield => {
@@ -106,8 +127,8 @@ function gdp_composition_by_end_use(category) {
 
     return {
         'end_uses': endUses,
-        'estimated': field.subfields[0].estimated,
-        'date': field.subfields[0].info_date,
+        'estimated': datedSubfield && datedSubfield.estimated,
+        'date': datedSubfield && datedSubfield.info_date,
         ...getNoteIfExists(field)
     };
 }
@@ -118,6 +139,10 @@ function gdp_composition_by_sector_of_origin(category) {
 
     if (!field) return null;
     if (!field.subfields) return transmuteHtmlToPlain(field.content);
+
+    // Ian's JSON presents only one date for the entire field
+    // Even though the dates may exist separately and independently
+    const datedSubfield = field.subfields.find(subfield => subfield.info_date);
 
     const sectors = Object.fromEntries(
         field.subfields
@@ -133,8 +158,42 @@ function gdp_composition_by_sector_of_origin(category) {
 
     return {
         'sectors': sectors,
-        'estimated': field.subfields[0].estimated,
-        'date': field.subfields[0].info_date,
+        'estimated': datedSubfield && datedSubfield.estimated,
+        'date': datedSubfield && datedSubfield.info_date,
+        ...getNoteIfExists(field)
+    };
+}
+
+
+function credit_ratings(category) {
+    const field = findFieldById(category, 237);
+
+    if (!field) return null;
+    if (!field.subfields) return transmuteHtmlToPlain(field.content);
+
+    const sfFitchRating = findSubfieldByName(field, 'Fitch rating');
+    const sfMoodysRating = findSubfieldByName(field, "Moody's rating");
+    const sfSAndPRating = findSubfieldByName(field, 'Standard & Poors rating');
+
+    return {
+        ...sfFitchRating && {
+            'fitch_rating': {
+                'rating': sfFitchRating.value,
+                'date': sfFitchRating.info_date
+            }
+        },
+        ...sfMoodysRating && {
+            'moodys_rating': {
+                'rating': sfMoodysRating.value,
+                'date': sfMoodysRating.info_date
+            }
+        },
+        ...sfSAndPRating && {
+            'standard_and_poor_rating': {
+                'rating': sfSAndPRating.value,
+                'date': sfSAndPRating.info_date
+            }
+        },
         ...getNoteIfExists(field)
     };
 }
@@ -177,6 +236,248 @@ function industrial_production_growth_rate(category) {
         'estimated': field.estimated,
         'date': field.info_date
     }
+}
+
+
+function labor_force_total_size(category) {
+    const field = findFieldById(category, 218);
+
+    if (!field) return null;
+
+    return {
+        'total_people': parseFloat(field.value),
+        'estimated': field.estimated,
+        'date': field.info_date,
+        ...getNoteIfExists(field)
+    }
+}
+
+
+function labor_force_by_occupation(category) {
+    const field = findFieldById(category, 219);
+
+    if (!field) return null;
+    if (!field.subfields) return transmuteHtmlToPlain(field.content);
+
+    // Ian's JSON presents only one date for the entire field
+    // Even though the dates may exist separately and independently
+    const datedSubfield = field.subfields.find(subfield => subfield.info_date);
+
+    const occupation = Object.fromEntries(
+        field.subfields
+             .map(subfield => {
+                const k = toSnakeCase(subfield.name);
+                const v = {
+                    'value': parseFloat(subfield.value),
+                    'units': subfield.suffix
+                };
+                return [k, v];
+             })
+    );
+
+    return {
+        'occupation': occupation,
+        'estimated': datedSubfield && datedSubfield.estimated,
+        'date': datedSubfield && datedSubfield.info_date,
+        ...getNoteIfExists(field)
+    };
+}
+
+
+function unemployment_rate_overall(category) {
+    const field = findFieldById(category, 220);
+
+    if (!field) return null;
+    if (!field.subfields) return transmuteHtmlToPlain(field.content);
+
+    return {
+        'annual_values': field.subfields.map(subfield => subfieldForOneYear(subfield)),
+        ...getNoteIfExists(field)
+    };
+
+}
+
+
+function unemployment_rate_in_youth_ages_15_to_24(category) {
+    const field = findFieldById(category, 373);
+
+    if (!field) return null;
+    if (!field.subfields) return transmuteHtmlToPlain(field.content);
+
+    // Ian's JSON presents only one date for the entire field
+    // Even though the dates may exist separately and independently
+    const datedSubfield = field.subfields.find(subfield => subfield.info_date);
+
+    const sections = Object.fromEntries(
+        field.subfields
+             .map(subfield => {
+                const k = toSnakeCase(subfield.name);
+                const v = {
+                    'value': parseFloat(subfield.value),
+                    'units': subfield.suffix
+                };
+                return [k, v];
+             })
+    );
+
+    return {
+        'sections': sections,
+        'estimated': datedSubfield && datedSubfield.estimated,
+        'date': datedSubfield && datedSubfield.info_date,
+        ...getNoteIfExists(field)
+    };
+}
+
+
+function population_below_poverty_line(category) {
+    const field = findFieldById(category, 221);
+
+    if (!field) return null;
+    return transmuteValueUnitDateField(field);
+}
+
+
+function gini_index_coefficient(category) {
+    const field = findFieldById(category, 223);
+
+    if (!field) return null;
+    if (!field.subfields) return transmuteHtmlToPlain(field.content);
+
+    return {
+        'annual_values': field.subfields.map(subfield => subfieldForOneYear(subfield)),
+        ...getNoteIfExists(field)
+    };
+}
+
+
+function household_income_by_percentage_share(category) {
+    const field = findFieldById(category, 222);
+
+    if (!field) return null;
+    if (!field.subfields) return transmuteHtmlToPlain(field.content);
+
+    // Ian's JSON presents only one date for the entire field
+    // Even though the dates may exist separately and independently
+    const datedSubfield = field.subfields.find(subfield => subfield.info_date);
+
+    const sections = Object.fromEntries(
+        field.subfields
+             .map(subfield => {
+                const k = toSnakeCase(subfield.name.replace('10%', 'ten percent'));
+                const v = {
+                    'value': parseFloat(subfield.value),
+                    'units': subfield.suffix
+                };
+                return [k, v];
+             })
+    );
+
+    return {
+        ...sections,
+        'estimated': datedSubfield && datedSubfield.estimated,
+        'date': datedSubfield && datedSubfield.info_date,
+        ...getNoteIfExists(field)
+    };
+
+}
+
+
+function budget(category) {
+    const field = findFieldById(category, 224);
+
+    if (!field) return null;
+    if (!field.subfields) return transmuteHtmlToPlain(field.content);
+
+    // Ian's JSON presents only one date for the entire field
+    // Even though the dates may exist separately and independently
+    const datedSubfield = field.subfields.find(subfield => subfield.info_date);
+
+    const sections = Object.fromEntries(
+        field.subfields
+             .map(subfield => {
+                const k = toSnakeCase(subfield.name);
+                const v = {
+                    'value': parseFloat(subfield.value),
+                    'units': subfield.suffix || 'USD'
+                };
+                return [k, v];
+             })
+    );
+
+    return {
+        ...sections,
+        'estimated': datedSubfield && datedSubfield.estimated,
+        'date': datedSubfield && datedSubfield.info_date,
+        ...getNoteIfExists(field)
+    };
+}
+
+
+function taxes_and_other_revenues(category) {
+    const field = findFieldById(category, 225);
+
+    if (!field) return null;
+    
+    return {
+        'percent_of_gdp': parseFloat(field.value),
+        'estimated': field.estimated,
+        'date': field.info_date,
+        ...getNoteIfExists(field)
+    };
+}
+
+
+function budget_surplus_or_deficit(category) {
+    const field = findFieldById(category, 226);
+
+    if (!field) return null;
+
+    return {
+        'percent_of_gdp': parseFloat(field.value),
+        'estimated': field.estimated,
+        'date': field.info_date,
+        ...getNoteIfExists(field)
+    };
+}
+
+
+function public_debt(category) {
+    const field = findFieldById(category, 227);
+
+    if (!field) return null;
+    if (!field.subfields) return transmuteHtmlToPlain(field.content);
+
+    return {
+        'annual_values': field.subfields.map(subfield => subfieldForOneYear(subfield)),
+        ...getNoteIfExists(field)
+    };
+}
+
+
+function fiscal_year(category) {
+    const field = findFieldById(category, 228);
+
+    if (!field) return null;
+    
+    const fromToRegex = /^(?<fromDate>\d+ \w+)\s+\-\s+(?<toDate>\d+ \w+)$/;
+    const calendarYearStr = 'calendar year';
+
+    if (field.value.toLowerCase() === calendarYearStr) {
+        return {
+            'start': '1 January',
+            'end': '31 December'
+        };
+    }
+
+    if (fromToRegex.test(field.value)) {
+        const match = fromToRegex.exec(field.value);
+        return {
+            'start': match.groups.fromDate,
+            'end': match.groups.toDate
+        };
+    }
+
+    return transmuteHtmlToPlain(field.content);
 }
 
 
